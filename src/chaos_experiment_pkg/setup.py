961fdbb51fd7db05ca9f99d6fc2e77038d72c5ea
BowenMichael <michael@thebowenfamily.com>
2026-03-22 19:25:48 -0400

    refactor: migrate chaos_experiment_pkg to poetry for dependency management

--- src/chaos_experiment_pkg/setup.py
+++ src/chaos_experiment_pkg/setup.py
@@ -7,29 +7,15 @@ package_name = 'chaos_experiment_pkg'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.1.0',
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-         (os.path.join('share', 'chaos_experiment_pkg', 'launch'),
-      glob('launch/*.launch.py'))
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='michael@thebowenfamily.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'data_logger = chaos_experiment_pkg.data_logger:main'
-        ],
-    },
+    # ALL OTHER METADATA REMOVED - IT LIVES IN pyproject.toml NOW
 )
